4963d1a394350793a542005ff2092f979b56cd2e
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2026-03-05 01:25:51 +0600

    Update .gitignore, modify API port, enhance FaceAnalysis initialization, and clean up requirements.txt

--- strans.py
+++ strans.py
@@ -1,7 +1,8 @@
 from sentence_transformers import SentenceTransformer
-import numpy as np 
+import numpy as np
+import torch
 
-model = SentenceTransformer('all-MiniLM-L6-v2', backend="onnx")
+model = SentenceTransformer('all-MiniLM-L6-v2', device='cuda' if torch.cuda.is_available() else 'cpu')
 
 def parsenp(obj):
   if isinstance(obj, np.ndarray):
