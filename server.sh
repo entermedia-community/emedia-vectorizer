0fb2b970a36ad776aa24881a7e73f7847e7323ef
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2026-03-05 01:40:08 +0600

    Add server.sh script to run Flask app with uvicorn

---
+++ server.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+set -eu
+
+HOST="${HOST:-0.0.0.0}"
+PORT="${PORT:-9600}"
+WORKERS="${WORKERS:-1}"
+
+# Run Flask app through uvicorn using its WSGI interface and app factory.
+exec uvicorn app:create_app \
+	--factory \
+	--interface wsgi \
+	--host "$HOST" \
+	--port "$PORT" \
+	--workers "$WORKERS"
