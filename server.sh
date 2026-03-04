6c22200fe161fa6df53a98c14dd5e067c8011ebb
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2026-03-05 01:47:42 +0600

    Set CUDA_VISIBLE_DEVICES environment variable before running uvicorn in server.sh

--- server.sh
+++ server.sh
@@ -5,7 +5,9 @@ HOST="${HOST:-0.0.0.0}"
 PORT="${PORT:-9600}"
 WORKERS="${WORKERS:-1}"
 
-exec CUDA_VISIBLE_DEVICES=0 uvicorn app:create_app \
+export CUDA_VISIBLE_DEVICES=0
+
+exec uvicorn app:create_app \
 	--factory \
 	--interface wsgi \
 	--host "$HOST" \
